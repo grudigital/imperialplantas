f92e9700af81baa7de2534aa9bffc199ce0d2140
contato@grudigital.com.br <2423wxci290989>
2020-08-13 12:55:40 -0300

    @felipesergio pagina duvidas e pagina interna

---
+++ depoimento_interna.php
@@ -0,0 +1,41 @@
+<?php include 'includes/header.php'?>
+<body class="blog-v2">
+<?php include 'includes/topo.php'?>
+<section class="banner-blog">
+    <div class="img_banner">
+        <h4 class="banner-h4">Depoimentos</h4>
+        <ul class="breadcrumb">
+            <li><a href="">Home</a></li>
+            <li class="active"><a href="">Depoimento</a></li>
+        </ul>
+    </div>
+</section>
+<!-- BLOG ZIGZAG -->
+<section class="blog-zigzag ">
+    <div class="container">
+        <div class="wpb_wrapper">
+            <div class="row">
+                <div class="item-zigzag clearfix">
+                    <div class="col-12" style="margin-top: -10px">
+                        <h3 class="oct_blog_title"><a style="font-size: 30px; margin-bottom: 60px" href="#">Aqui virá o titulo da duvida </a></h3>
+                    </div>
+                </div>
+                <div class="item-zigzag clearfix">
+                    <div class="col-12">
+                        <p style="font-size: 20px; margin-top: -50px" class="k_blog_content">
+                            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry’s standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled it to make some type specimen book. It has survived not only five centuries, but also...
+                            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry’s standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled it to make some type specimen book. It has survived not only five centuries, but also...
+                            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry’s standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled it to make some type specimen book. It has survived not only five centuries, but also...
+                            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry’s standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled it to make some type specimen book. It has survived not only five centuries, but also...
+                            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry’s standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled it to make some type specimen book. It has survived not only five centuries, but also...
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include 'includes/rodape.php'?>
+<?php include 'includes/scriptsrodape.php'?>
+</body>
+</html>
---
+++ duvidas.php
@@ -0,0 +1,110 @@
+<?php include 'includes/header.php'?>
+<body class="blog-v2">
+<?php include 'includes/topo.php'?>
+<section class="banner-blog">
+    <div class="img_banner">
+        <h4 class="banner-h4">Dúvidas</h4>
+        <ul class="breadcrumb">
+            <li><a href="">Home</a></li>
+            <li class="active"><a href="">Suas dúvidas</a></li>
+        </ul>
+    </div>
+</section>
+
+<section class="body-blog-listing">
+    <div class="container">
+        <div class="item-blog clearfix">
+            <div class="info">
+                <div class="oct_comment">
+
+                </div>
+                <a class="title" href="depoimento_interna.php" title="Esse é um depoimento">Essa é uma dúvida do cliente</a>
+            </div>
+            <div class="date">
+                <p class="time-post">22&nbsp;<span>Agosto</span></p>
+            </div>
+        </div>
+        <div class="item-blog clearfix">
+            <div class="info">
+                <div class="oct_comment">
+
+                </div>
+                <a class="title" href="depoimento_interna.php" title="Esse é um depoimento">Essa é uma dúvida do cliente</a>
+            </div>
+            <div class="date">
+                <p class="time-post">01&nbsp;<span>Agosto</span></p>
+            </div>
+        </div>
+        <div class="item-blog clearfix">
+            <div class="info">
+                <div class="oct_comment">
+
+                </div>
+                <a class="title" href="depoimento_interna.php" title="Esse é um depoimento">Essa é uma dúvida do cliente</a>
+            </div>
+            <div class="date">
+                <p class="time-post">03&nbsp;<span>Agosto</span></p>
+            </div>
+        </div>
+        <div class="item-blog clearfix">
+            <div class="info">
+                <div class="oct_comment">
+
+                </div>
+                <a class="title" href="depoimento_interna.php" title="Esse é um depoimento">Essa é uma dúvida do cliente</a>
+            </div>
+            <div class="date">
+                <p class="time-post">05&nbsp;<span>Agosto</span></p>
+            </div>
+        </div>
+        <div class="item-blog clearfix">
+            <div class="info">
+                <div class="oct_comment">
+
+                </div>
+                <a class="title" href="depoimento_interna.php" title="Esse é um depoimento">Essa é uma dúvida do cliente</a>
+            </div>
+            <div class="date">
+                <p class="time-post">16&nbsp;<span>Agosto</span></p>
+            </div>
+        </div>
+        <div class="item-blog clearfix">
+            <div class="info">
+                <div class="oct_comment">
+
+                </div>
+                <a class="title" href="depoimento_interna.php" title="Esse é um depoimento">Essa é uma dúvida do cliente</a>
+            </div>
+            <div class="date">
+                <p class="time-post">30&nbsp;<span>Agosto</span></p>
+            </div>
+        </div>
+        <div class="item-blog clearfix">
+            <div class="info">
+                <div class="oct_comment">
+
+                </div>
+                <a class="title" href="depoimento_interna.php" title="Esse é um depoimento">Essa é uma dúvida do cliente</a>
+            </div>
+            <div class="date">
+                <p class="time-post">19&nbsp;<span>Agosto</span></p>
+            </div>
+        </div>
+        <div class="item-blog clearfix">
+            <div class="info">
+                <div class="oct_comment">
+
+                </div>
+                <a class="title" href="depoimento_interna.php" title="Esse é um depoimento">Essa é uma dúvida do cliente</a>
+            </div>
+            <div class="date">
+                <p class="time-post">26&nbsp;<span>Agosto</span></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'includes/rodape.php'?>
+<?php include 'includes/scriptsrodape.php'?>
+</body>
+</html>
