b592700974d1b9aef6eccf9e244f099502473e0f
contato@grudigital.com.br <2423wxci290989>
2020-08-13 12:40:10 -0300

    @felipesergio página produtos e serviços e página interna de produtos e serviços

---
+++ produtoseservicos.php
@@ -0,0 +1,198 @@
+<?php include 'includes/header.php'?>
+<body class="about-page">
+<?php include 'includes/topo.php'?>
+<section id="breadcrumb">
+    <div class="container-fluid">
+        <div class="breadcrumb-content bg-breadcrumb-04">
+            <div class="caption">
+                <div class="title">Produtos e serviços</div>
+                <ul>
+                    <li>Home</li>
+                    <li>Produtos e serviços</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="product">
+    <div class="  wrap-blog   ">
+        <div class="container">
+
+            <div class="gallery gallery-restaurant clearfix">
+                <!-- FILTER -->
+                <div class="gallery-cat text-center ">
+                    <ul class="list-inline ">
+                        <li class="active"><a style="font-size: 20px; font-weight: bold" href="#" data-filter="*">Todos</a></li>
+                        <li><a href="#" style="font-size: 20px" data-filter=".ground">Produtos </a></li>
+                        <li><a href="#" style="font-size: 20px" data-filter=".suite">Serviços </a></li>
+
+                    </ul>
+                </div>
+                <!-- END / FILTER -->
+                <!-- GALLERY CONTENT -->
+                <div class="gallery-content">
+                    <div class="row">
+                        <div class="gallery-isotope col-5 ">
+                            <!-- ITEM SIZE -->
+                            <div class="item-size "></div>
+                            <!-- END / ITEM SIZE -->
+                            <!-- ITEM -->
+                            <div class="item-isotope suite dining design">
+                                <div class="wrap-box-1">
+                                    <div class="box-img">
+                                        <a href="produtoseservicos_interna.php">
+                                            <img src="images/product/pro_09.jpg" class="img-responsive" alt="Product" title="images products">
+                                        </a>
+                                        <div class="content-item">
+                                            <h5 class="title-h5"><a href="produtoseservicos_interna.php">Titulo do serviço</a></h5>
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END / ITEM -->
+                            <!-- ITEM -->
+                            <div class="item-isotope suite dining design">
+                                <div class="wrap-box-1">
+                                    <div class="box-img">
+                                        <a href="produtoseservicos_interna.php">
+                                            <img src="images/product/pro_09.jpg" class="img-responsive" alt="Product" title="images products"></a>
+                                        <div class="content-item">
+                                            <h5 class="title-h5"><a href="produtoseservicos_interna.php">Titulo do serviço</a></h5>
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END / ITEM -->
+                            <!-- ITEM -->
+                            <div class="item-isotope suite dining design">
+                                <div class="wrap-box-1">
+                                    <div class="box-img">
+                                        <a href="produtoseservicos_interna.php">
+                                            <img src="images/product/pro_09.jpg" class="img-responsive" alt="Product" title="images products"></a>
+                                        <div class="content-item">
+                                            <h5 class="title-h5"><a href="produtoseservicos_interna.php">Titulo do serviço</a></h5>
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END / ITEM -->
+                            <!-- ITEM -->
+                            <div class="item-isotope suite dining design">
+                                <div class="wrap-box-1">
+                                    <div class="box-img">
+                                        <a href="produtoseservicos_interna.php">
+                                            <img src="images/product/pro_09.jpg" class="img-responsive" alt="Product" title="images products"></a>
+                                        <div class="content-item">
+                                            <h5 class="title-h5"><a href="produtoseservicos_interna.php">Titulo do serviço</a></h5>
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END / ITEM -->
+                            <!-- ITEM -->
+                            <div class="item-isotope  ground  dining">
+                                <div class="wrap-box-1">
+                                    <div class="box-img">
+                                        <a href="produtoseservicos_interna.php">
+                                            <img src="images/product/pro_08.jpg" class="img-responsive" alt="Product" title="images products"></a>
+                                        <div class="content-item">
+                                            <h5 class="title-h5"><a href="produtoseservicos_interna.php">Titulo do produto</a></h5>
+                                            <div class="bottom">
+                                                <div class="text-left pull-left">
+                                                    <span class="price">R$ 350.00</span>
+                                                </div>
+                                                <div class="text-right">
+                                                    <span class="height">133 cm</span>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END / ITEM -->
+                            <!-- ITEM -->
+                            <div class="item-isotope  ground bathroom dining">
+                                <div class="wrap-box-1">
+                                    <div class="box-img">
+                                        <a href="produtoseservicos_interna.php">
+                                            <img src="images/product/pro_08.jpg" class="img-responsive" alt="Product" title="images products"></a>
+                                        <div class="content-item">
+                                            <h5 class="title-h5"><a href="produtoseservicos_interna.php">Titulo do produto</a></h5>
+                                            <div class="bottom">
+                                                <div class="text-left pull-left">
+                                                    <span class="price">R$ 350.00</span>
+                                                </div>
+                                                <div class="text-right">
+                                                    <span class="height">133 cm</span>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END / ITEM -->
+                            <!-- ITEM -->
+                            <div class="item-isotope  ground bathroom dining">
+                                <div class="wrap-box-1">
+                                    <div class="box-img">
+                                        <a href="produtoseservicos_interna.php">
+                                            <img src="images/product/pro_08.jpg" class="img-responsive" alt="Product" title="images products"></a>
+                                        <div class="content-item">
+                                            <h5 class="title-h5"><a href="produtoseservicos_interna.php">Titulo do produto</a></h5>
+                                            <div class="bottom">
+                                                <div class="text-left pull-left">
+                                                    <span class="price">R$ 350.00</span>
+                                                </div>
+                                                <div class="text-right">
+                                                    <span class="height">133 cm</span>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="item-isotope ground dining bathroom">
+                                <div class="wrap-box-1">
+                                    <div class="box-img">
+                                        <a href="produtoseservicos_interna.php">
+                                            <img src="images/product/pro_08.jpg" class="img-responsive" alt="Product" title="images products"></a>
+                                        <div class="content-item">
+                                            <h5 class="title-h5"><a href="produtoseservicos_interna.php">Titulo do produto</a></h5>
+                                            <div class="bottom">
+                                                <div class="text-left pull-left">
+                                                    <span class="price">R$ 350.00</span>
+                                                </div>
+                                                <div class="text-right">
+                                                    <span class="height">133 cm</span>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END / ITEM -->
+                        </div>
+                    </div>
+                </div>
+                <!-- GALLERY CONTENT -->
+            </div>
+        </div>
+    </div>
+</section>
+<?php include 'includes/rodape.php'?>
+<?php include 'includes/scriptsrodape.php'?>
+</body>
+</html>
---
+++ produtoseservicos_interna.php
@@ -0,0 +1,48 @@
+<?php include 'includes/header.php'?>
+<body class="blog-v2">
+<?php include 'includes/topo.php'?>
+<section class="banner-blog">
+    <div class="img_banner">
+        <h4 class="banner-h4">Produtos e serviços</h4>
+        <ul class="breadcrumb">
+            <li><a href="">Home</a></li>
+            <li><a href="">Produtos e serviços</a></li>
+            <li class="active"><a href="">Nome do produto ou serviço</a></li>
+        </ul>
+    </div>
+</section>
+<!-- BLOG ZIGZAG -->
+<section class="blog-zigzag ">
+    <div class="container">
+        <div class="wpb_wrapper">
+            <div class="row">
+                <div class="item-zigzag clearfix">
+                    <div class="col-12" style="margin-top: -40px">
+                        <h3 class="oct_blog_title"><a style="font-size: 30px; margin-bottom: 60px" href="#">Nome do produto ou serviço </a></h3>
+                        <br/><br/>
+                        <div class="post-standard">
+                            <a href="#">
+                                <figure><img src="images/blog/internaprodutoseservicos.jpg" alt="Lorem Ipsum is simply dummy text of the printing"></figure>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="item-zigzag clearfix">
+                    <div class="col-12">
+                        <p style="font-size: 20px; margin-top: -50px" class="k_blog_content">
+                            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry’s standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled it to make some type specimen book. It has survived not only five centuries, but also...
+                            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry’s standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled it to make some type specimen book. It has survived not only five centuries, but also...
+                            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry’s standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled it to make some type specimen book. It has survived not only five centuries, but also...
+                            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry’s standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled it to make some type specimen book. It has survived not only five centuries, but also...
+                            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry’s standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled it to make some type specimen book. It has survived not only five centuries, but also...
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include 'includes/rodape.php'?>
+<?php include 'includes/scriptsrodape.php'?>
+</body>
+</html>
