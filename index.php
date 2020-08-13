244529e1b5466018b3424d87865c3455697e097a
Netto <netto@grudigital.com.br>
2020-08-12 23:37:08 -0300

    @netto - Adicionado pagina Quem somos e contato > Falta adicionar imagens

--- index.php
+++ index.php
@@ -688,7 +688,13 @@
 <!-- Custom jQuery -->
 <script type="text/javascript" src="js/function.js"></script>
 
-
+<!-----Inicio WhatsApp----->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://wa.me/5511980246313?text=Adorei%20seu%20artigo" style="position:fixed;width:60px;height:60px;bottom:15px;left:10px;background-color:#80b435;color:#FFF;border-radius:50px;text-align:center;font-size:30px;box-shadow: 1px 1px 2px #888;
+  z-index:1000;" target="_blank">
+    <i style="margin-top:16px" class="fa fa-whatsapp"></i>
+</a>
+<!-----Fim WhatsApp----->
 
 
 </body>
